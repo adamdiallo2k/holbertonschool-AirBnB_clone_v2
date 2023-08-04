d073fb109e0c361b7cd4c89d675172a9c1307f24
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2023-08-04 22:56:15 +0200

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,34 +1,53 @@
-import unittest
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import unittest
 
 
-class TestAmenity(unittest.TestCase):
-    def test_attributes(self):
-        """
-        Test that the Amenity class has the correct attributes
-        """
-        amenity = Amenity()
-        self.assertTrue(hasattr(amenity, 'id'))
-        self.assertTrue(hasattr(amenity, 'created_at'))
-        self.assertTrue(hasattr(amenity, 'updated_at'))
-        self.assertTrue(hasattr(amenity, 'name'))
+class test_Amenity(unittest.TestCase):
+    """ tests for Amenity class """
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.obj = Amenity()
+        cls.obj.name = "Wifi"
 
-    def test_name_type(self):
-        """
-        Test that the name attribute is of type string
-        """
-        amenity = Amenity()
-        name_type = type(amenity.name)
-        self.assertEqual(name_type, str)
+    @classmethod
+    def tearDown(self):
+        """ removes json file """
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_str_method(self):
-        """
-        Test that the __str__ method returns the expected string
-        """
-        amenity = Amenity()
-        expected_str = "[Amenity] ({}) {}".format(amenity.id, amenity.__dict__)
-        self.assertEqual(str(amenity), expected_str)
+    def type_obj(self):
+        """ test type """
+        self.assertEqual(type(self.obj.name), str)
+
+    def is_subclass(self):
+        """ tests subclass of BaseModel """
+        self.assertTrue(issubclass(self.obj.__class__, BaseModel), True)
+
+    def dic_save(self):
+        obj = Place()
+        obj.name = "Flat"
+        place = obj.to_dict
+        self.assertTrue(type(place), dict)
+
+"""
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
+
+    def test_name2(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
+"""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

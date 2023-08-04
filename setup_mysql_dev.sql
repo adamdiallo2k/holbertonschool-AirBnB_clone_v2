b20ccd952e65c5d59959bcc5af79f9b3488b3924
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-08-04 03:42:49 +0200

    fix sql request

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
 -- Script that prepares a MySQL server for the project:
 -- Creates the database hbnb_dev_db and the user hbnb_dev
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS hbnb_test@localhost;
+FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';

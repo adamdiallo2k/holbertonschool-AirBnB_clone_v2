9f1c16c6b641d5fa546292dd242422851117d1cc
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-16 17:30:40 +0200

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,16 +21,21 @@ def display_text(text):
     text_with_spaces = text.replace("_", " ")
     return f'C {text_with_spaces}'
 
-@app.route("/c/<text>", strict_slashes=False)
-def display_text(text):
-    """Returns a string with 'C' followed by the captured text."""
-    text_with_spaces = text.replace("_", " ")
-    return f'C {text_with_spaces}'
 
 @app.route("/hbnb", strict_slashes=False)
 def about():
     """Returns a string for the root route."""
     return "HBNB"
 
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text):
+    """Display 'Python', followed by the value of the text variable with underscores replaced by spaces."""
+    text_with_spaces = text.replace("_", " ")
+    return f'Python {text_with_spaces}'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

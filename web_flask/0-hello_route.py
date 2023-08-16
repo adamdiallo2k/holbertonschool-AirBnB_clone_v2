91efd248b20065f629872253e1df4481561e5649
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-16 16:09:47 +0200

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-This module starts a 
+This module starts a
 Flask web application with the following specifications:
 """
 
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_world():

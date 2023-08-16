09d4382c721fddba75573ca0d8e89b7fd3eca54d
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-16 16:45:24 +0200

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module starts a
+Flask web application with the following specifications:
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Returns a string for the root route."""
+    return "Hello HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """Returns a string with 'C' followed by the captured text."""
+    return f'C {text}'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def about():
+    """Returns a string for the root route."""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

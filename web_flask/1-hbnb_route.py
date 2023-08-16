4efa67b0a5b51a935a57c4d94604231e92611faf
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-16 16:29:46 +0200

    test

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,6 +14,7 @@ def hello_world():
     """Returns a string for the root route."""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def about():
     """Returns a string for the root route."""

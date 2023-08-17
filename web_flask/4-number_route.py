f10f143e714dc45e7e3a3d033d2a27affcdee9b2
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-17 10:40:12 +0200

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This module starts a
+Flask web application with the following specifications:
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Returns a string for the root route."""
+    return "Hello HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """Returns a string with 'C' followed by the captured text."""
+    text_with_spaces = text.replace("_", " ")
+    return f'C {text_with_spaces}'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def about():
+    """Returns a string for the root route."""
+    return "HBNB"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text):
+    """Display 'Python', followed by the value of the text
+    variable with underscores replaced by spaces."""
+    text_with_spaces = text.replace("_", " ")
+    return f'Python {text_with_spaces}'
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    """display “n is a number” only if n is an integer"""
+    if n.isdigit():
+        return f'{n} is a number'
+
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5000)

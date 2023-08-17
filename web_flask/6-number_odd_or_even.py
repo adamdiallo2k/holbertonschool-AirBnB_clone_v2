cd6dc93e7fda85b8e9290b7ba97ad73b62b985ff
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-17 18:36:56 +0200

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -58,7 +58,7 @@ def numberTemp(n):
 def numberOddev(n):
     """display “n is a number” only if n is an integer"""
     if n.isdigit():
-        return render_template('6-number_odd_or_even.html', n=n)
+        return render_template('6-number_odd_or_even.html', n=int(n))
     else:
         return abort(404)
 

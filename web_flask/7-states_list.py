0f2b1e70e6b49cd4f351145c5a439eae02657455
Adam <adamdiallo2k@gmail.com>
2023-08-18 10:33:06 -0700

    test

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+This module starts a
+Flask web application with the following specifications:
+"""
+from models import storage and storage.all(...)
+
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the storage after each request."""
+    storage.close()
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Returns a string for the root route."""
+    return "Hello HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """Returns a string with 'C' followed by the captured text."""
+    text_with_spaces = text.replace("_", " ")
+    return f'C {text_with_spaces}'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def about():
+    """Returns a string for the root route."""
+    return "HBNB"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text):
+    """Display 'Python', followed by the value of the text
+    variable with underscores replaced by spaces."""
+    text_with_spaces = text.replace("_", " ")
+    return f'Python {text_with_spaces}'
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    """display “n is a number” only if n is an integer"""
+    if n.isdigit():
+        return f'{n} is a number'
+    else:
+        return abort(404)
+
+
+@app.route("/number_template/<n>", strict_slashes=False)
+def numberTemp(n):
+    """display “n is a number” only if n is an integer"""
+    if n.isdigit():
+        return render_template('5-number.html', n=n)
+    else:
+        return abort(404)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def numberOddev(n):
+    """display “n is a number” only if n is an integer"""
+    return render_template('6-number_odd_or_even.html', num=n)
+
+@app.route('/states_list')
+def states_list():
+    """Display a list of all State objects sorted by name."""
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('states_list.html', states=sorted_states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
